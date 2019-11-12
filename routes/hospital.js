c13b5c0e8b412869fa88bcfef8919e7104a9b4e9
Leandro David Paschiero <leandro_6315@hotmail.com>
2019-11-12 09:21:32 -0300

    Obtener hospital por id

--- routes/hospital.js
+++ routes/hospital.js
@@ -37,6 +37,37 @@ app.get('/',(req,res) => {
     });
 });
 
+//========================================
+//Peticion GET por id
+//========================================
+app.get('/:id',(req,res) => {
+    var id = req.params.id;
+
+    Hospital.findById(id, 'nombre img')
+    .exec((err, hospital) => {
+
+        if(err){
+            return res.status(500).json({
+                ok: false,
+                mensaje: "Error al buscar hospital",
+                errors: err
+            })
+        }
+
+        if(!hospital){
+            return res.status(400).json({
+                ok: false,
+                mensaje: "La busqueda no arrojo resultados",
+                errors: { message: 'No existe un hospital con ese ID' }
+            });
+        }
+
+        res.status(200).json({
+            ok: true,
+            hospital: hospital
+        })
+    });
+});
 
 //========================================
 //Peticion POST
